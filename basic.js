0e46276d0f987b87b0bab3a49e861b97566b512e
Shashank M <shashankmish27@gmail.com>
2025-05-09 16:13:08 +0300

    ver2

--- basic.js
+++ basic.js
@@ -1,8 +1,15 @@
 const colors = require('colors')
+const cowsay = require('cowsay')
 
 const helloWorld = () => {
 
-    console.log("Hey man".bgGreen.red)
+    console.log(
+        cowsay.say({
+            text: 'hey man!',
+            e: 'xx',
+            r: true,
+        })
+    )
 }
 
 helloWorld()
